5c18993ddb1c55bb38cb066dd7039ae7516d9324
gota morishita <optimistic.gota@gmail.com>
2020-03-28 16:51:50 +0900

    update perceptron.py

--- ch2/perceptron.py
+++ ch2/perceptron.py
@@ -36,7 +36,6 @@ assert AND_bias(0, 1) == 0, 'expected 0, but output is {AND_bias(0, 1)}'
 assert AND_bias(1, 1) == 1, 'expected 1, but output is {AND_bias(1, 1)}'
 
 
-# NAND
 def NAND(x1, x2):
     x = np.array([x1, x2])
     w = np.array([-0.5, -0.5])
@@ -49,7 +48,39 @@ def NAND(x1, x2):
 
 
 # check if NAND works properly
-assert NAND(0, 0) == 1, 'expected 1, but output is {AND(0, 0)}'
-assert NAND(1, 0) == 1, 'expected 1, but output is {AND(1, 0)}'
-assert NAND(0, 1) == 1, 'expected 1, but output is {AND(0, 1)}'
-assert NAND(1, 1) == 0, 'expected 0, but output is {AND(1, 1)}'
+assert NAND(0, 0) == 1, 'expected 1, but output is {NAND(0, 0)}'
+assert NAND(1, 0) == 1, 'expected 1, but output is {NAND(1, 0)}'
+assert NAND(0, 1) == 1, 'expected 1, but output is {NAND(0, 1)}'
+assert NAND(1, 1) == 0, 'expected 0, but output is {NAND(1, 1)}'
+
+
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.3, 0.3])
+    b = -0.2
+    tmp = w @ x + b
+    if (tmp <= 0):
+        return 0
+    else:
+        return 1
+
+
+# check if OR works properly
+assert OR(0, 0) == 0, 'expected 1, but output is {OR(0, 0)}'
+assert OR(1, 0) == 1, 'expected 1, but output is {OR(1, 0)}'
+assert OR(0, 1) == 1, 'expected 1, but output is {OR(0, 1)}'
+assert OR(1, 1) == 1, 'expected 0, but output is {OR(1, 1)}'
+
+
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+    return y
+
+
+# check if XOR works properly
+assert XOR(0, 0) == 0, 'expected 0, but output is {XOR(0, 0)}'
+assert XOR(1, 0) == 1, 'expected 1, but output is {XOR(1, 0)}'
+assert XOR(0, 1) == 1, 'expected 1, but output is {XOR(0, 1)}'
+assert XOR(1, 1) == 0, 'expected 0, but output is {XOR(1, 1)}'
