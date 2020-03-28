eb26c70c30e4d47e1a760a3f9a86dbb70da82dfc
gota morishita <optimistic.gota@gmail.com>
2020-03-28 16:35:54 +0900

    implement AND with bias

--- ch2/perceptron.py
+++ ch2/perceptron.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 # implementation of AND using perceptron
 def AND(x1, x2):
     w1, w2, theta = 0.5, 0.5, 0.7
@@ -13,3 +16,20 @@ assert AND(1, 0) == 0, 'expected 0, but output is {AND(1, 0)}'
 assert AND(0, 1) == 0, 'expected 0, but output is {AND(0, 1)}'
 assert AND(1, 1) == 1, 'expected 1, but output is {AND(1, 1)}'
 
+
+def AND_bias(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    tmp = w @ x + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+
+# check if AND_bias works properly
+assert AND_bias(0, 0) == 0, 'expected 0, but output is {AND_bias(0, 0)}'
+assert AND_bias(1, 0) == 0, 'expected 0, but output is {AND_bias(1, 0)}'
+assert AND_bias(0, 1) == 0, 'expected 0, but output is {AND_bias(0, 1)}'
+assert AND_bias(1, 1) == 1, 'expected 1, but output is {AND_bias(1, 1)}'
